772d0517fba3a19cfe09ffb7d726e89811de7c31
8borane8 <74107604+8borane8@users.noreply.github.com>
2024-12-20 22:21:32 +0100

    fix

--- src/server/Dom.ts
+++ src/server/Dom.ts
@@ -53,7 +53,7 @@ export class Dom {
         <title>${this.page.title}</title>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         ${styles.join("\n        ")}
         <link rel="icon shortcut" href="${this.page.favicon}" type="image/x-icon">
         ${pHead}
